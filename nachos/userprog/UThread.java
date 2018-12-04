ba20d4e1b6dc7110305f2e5128a09c1d1381b549
shawn <kliang8@ucmerced.edu>
2018-12-04 01:12:41 -0800

    Add files via upload

--- nachos/userprog/UThread.java
+++ nachos/userprog/UThread.java
@@ -1,151 +1,75 @@
 package nachos.userprog;
 
-import java.util.LinkedList;
-
 import nachos.machine.*;
 import nachos.threads.*;
+import nachos.userprog.*;
 
 /**
- * A kernel that can support multiple user processes.
- */
-public class UserKernel extends ThreadedKernel {
-	/**
-	 * Allocate a new user kernel.
-	 */
-	public UserKernel() {
-		super();
-	}
+ * A UThread is KThread that can execute user program code inside a user
+ * process, in addition to Nachos kernel code.
+ */ 
+public class UThread extends KThread {
+    /**
+     * Allocate a new UThread.
+     */
+    public UThread(UserProcess process) {
+	super();
 
-	/**
-	 * Initialize this kernel. Creates a synchronized console and sets the
-	 * processor's exception handler.
-	 */
-	public void initialize(String[] args) {
-		super.initialize(args);
-
-		console = new SynchConsole(Machine.console());
-
-		listLock = new Lock();
-		freePages = new LinkedList<Integer>();
-		for (int i = 0; i < Machine.processor().getNumPhysPages(); i++)
-			freePages.add(i);
-
-		Machine.processor().setExceptionHandler(new Runnable() {
-			public void run() {
-				exceptionHandler();
-			}
-		});
-	}
-
-	/**
-	 * Test the console device.
-	 */
-	public void selfTest() {
-		// super.selfTest();
-		//
-		// System.out.println("Testing the console device. Typed characters");
-		// System.out.println("will be echoed until q is typed.");
-		//
-		// char c;
-		//
-		// do {
-		// c = (char) console.readByte(true);
-		// console.writeByte(c);
-		// } while (c != 'q');
-		//
-		// System.out.println("");
-	}
-
-	/**
-	 * Returns the current process.
-	 * 
-	 * @return the current process, or <tt>null</tt> if no process is current.
-	 */
-	public static UserProcess currentProcess() {
-		if (!(KThread.currentThread() instanceof UThread))
-			return null;
-
-		return ((UThread) KThread.currentThread()).process;
-	}
-
-	/**
-	 * The exception handler. This handler is called by the processor whenever a
-	 * user instruction causes a processor exception.
-	 * 
-	 * <p>
-	 * When the exception handler is invoked, interrupts are enabled, and the
-	 * processor's cause register contains an integer identifying the cause of
-	 * the exception (see the <tt>exceptionZZZ</tt> constants in the
-	 * <tt>Processor</tt> class). If the exception involves a bad virtual
-	 * address (e.g. page fault, TLB miss, read-only, bus error, or address
-	 * error), the processor's BadVAddr register identifies the virtual address
-	 * that caused the exception.
-	 */
-	public void exceptionHandler() {
-		Lib.assertTrue(KThread.currentThread() instanceof UThread);
-
-		UserProcess process = ((UThread) KThread.currentThread()).process;
-		int cause = Machine.processor().readRegister(Processor.regCause);
-		process.handleException(cause);
-	}
-
-	/**
-	 * Start running user programs, by creating a process and running a shell
-	 * program in it. The name of the shell program it must run is returned by
-	 * <tt>Machine.getShellProgramName()</tt>.
-	 * 
-	 * @see nachos.machine.Machine#getShellProgramName
-	 */
-	public void run() {
-		super.run();
-
-		UserProcess process = UserProcess.newUserProcess();
-		rootProcess = process;
-
-		String shellProgram = Machine.getShellProgramName();
-		Lib.assertTrue(process.execute(shellProgram, new String[] {}));
-
-		KThread.finish();
-	}
-
-	/**
-	 * Terminate this kernel. Never returns.
-	 */
-	public void terminate() {
-		super.terminate();
-	}
-
-	public static int[] allocatePages(int num) {
-		listLock.acquire();
-
-		if (freePages.size() < num) {
-			listLock.release();
-			return null;
+	setTarget(new Runnable() {
+		public void run() {
+		    runProgram();
 		}
+	    });
 
-		int[] result = new int[num];
+	this.process = process;
+    }
 
-		for (int i = 0; i < num; i++)
-			result[i] = freePages.remove();
+    private void runProgram() {
+	process.initRegisters();
+	process.restoreState();
 
-		listLock.release();
+	Machine.processor().run();
+	
+	Lib.assertNotReached();
+    }
+    
+    /**
+     * Save state before giving up the processor to another thread.
+     */
+    protected void saveState() {
+	process.saveState();
 
-		return result;
-	}
+	for (int i=0; i<Processor.numUserRegisters; i++)
+	    userRegisters[i] = Machine.processor().readRegister(i);
 
-	public static void releasePage(int ppn) {
-		listLock.acquire();
-		freePages.add(ppn);
-		listLock.release();
-	}
+	super.saveState();
+    }
 
-	/** Globally accessible reference to the synchronized console. */
-	public static SynchConsole console;
+    /**
+     * Restore state before receiving the processor again.
+     */      
+    protected void restoreState() {
+	super.restoreState();
+	
+	for (int i=0; i<Processor.numUserRegisters; i++)
+	    Machine.processor().writeRegister(i, userRegisters[i]);
+	
+	process.restoreState();
+    }
 
-	/** Globally accessible reference to the root process. */
-	public static UserProcess rootProcess = null;
+    /**
+     * Storage for the user register set.
+     *
+     * <p>
+     * A thread capable of running user code actually has <i>two</i> sets of
+     * CPU registers: one for its state while executing user code, and one for
+     * its state while executing kernel code. While this thread is not running,
+     * its user state is stored here.
+     */
+    public int userRegisters[] = new int[Processor.numUserRegisters];
 
-	/** A global linked list of free physical pages. */
-	public static LinkedList<Integer> freePages;
-	public static Lock listLock;
+    /**
+     * The process to which this thread belongs.
+     */
+    public UserProcess process;
 }
